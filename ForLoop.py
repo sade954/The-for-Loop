e4e3bc01b318aa4766dba9ff7c5f64f4f1048e51
Candace Hollinger <csig1985@gmail.com>
2022-12-20 04:17:43 +0000

    update forloop

---
+++ ForLoop.py
@@ -0,0 +1,46 @@
+
+colors = ['blue', 'green', 'red', 'purple']
+for color in colors:
+    print(color)
+
+blue
+green
+red
+purple
+
+
+point = (1, 2, 3)
+for value in point:
+    print(value)
+    
+1
+2
+3
+ages = {'Candace': 37, 'Daniel':31, 'Journee': 21}
+for key in ages:
+    print(key)
+ 
+Candace
+Daniel
+Journee
+
+for key, value in ages.items()
+    print(key, value)
+
+Candace 37
+Daniel 31
+Journee 21
+
+
+for letter in 'my_string':
+    print(letter)
+ 
+m
+y
+_
+s
+t
+r
+i
+n
+g
